cb2727b4999f432d47d4a060590a1ca9c228275f
Daniel Teixeira Pereira <97935464+danielpereira91@users.noreply.github.com>
2024-02-05 16:02:24 -0300

    updated views urlpatterns

--- ayamo_refunds/urls.py
+++ ayamo_refunds/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from ayamo_refunds.views import home, contato, sobre
+from . import views
 
 urlpatterns = [
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('', views.home)
 ]    
